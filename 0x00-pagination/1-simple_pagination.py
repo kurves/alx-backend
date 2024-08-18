9c512133f0dc4f49aa139eed2b75848b83d6deb2
kurvs <kavssimply@gmail.com>
2024-08-18 20:24:04 +0000

    index pagination  function

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,34 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
+    
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Returns a tuple of size two containing a start index and an end index
+        """
+        start_index = (page - 1) * page_size
+        end_index = page * page_size
+        return start_index, end_index
