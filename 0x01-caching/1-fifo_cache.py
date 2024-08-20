429fbd9517bd9c6be4657affb15ea19df9cc258b
kurvs <kavssimply@gmail.com>
2024-08-20 20:02:35 +0000

    first in klast out caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a caching system that follows FIFO rule """
+
+    def __init__(self):
+        """ Initialize the class with the parent's initialization """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache following FIFO rule """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = self.order.pop(0)
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
